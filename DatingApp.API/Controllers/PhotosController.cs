69c91a1b4766032f1df2d0fa2bc4f8abf2b2b6ce
Christopher Waters <cwaters@Costargroup.com>
2019-11-01 14:33:01 -0400

    Cloudinary Phot storage

---
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -0,0 +1,10 @@
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/users/{userId}/phots")]
+    [ApiController]
+    public class PhotosController: ControllerBase
+    {
+        
+    }
+}
